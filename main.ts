08cc556992f5db8b49843ee82f646ad9002b0633
CuborubyPro <64404946+CuborubyPro@users.noreply.github.com>
2023-04-20 18:27:01 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "RRL") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, value)
+    } else if (name == "RRR") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, value)
+    } else if (name == "RRLb") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, value)
+    } else if (name == "RRRb") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, value)
+    }
+})
+let RRD = 0
+let RRTr = 0
+let RRTl = 0
+radio.setGroup(55)
+basic.forever(function () {
+    RRTl = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+    RRTr = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    RRD = maqueen.Ultrasonic(PingUnit.Centimeters)
+    serial.writeLine("" + (RRD))
+    radio.sendValue("RRTl", RRTl)
+    radio.sendValue("RRTr", RRTr)
+    radio.sendValue("RRD", RRD)
+})
